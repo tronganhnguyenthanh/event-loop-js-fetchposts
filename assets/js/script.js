1a15fee2dca65e73afec55dd087aa9deba6a38f3
Trong Anh Nguyen Thanh <tronganhnguyen222@gmail.com>
2025-08-18 17:58:12 +0700

    [UPDATE][FE] fixed input width

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,12 +34,14 @@ document.querySelector(".btn-seeMore").addEventListener("click", function(){
   const value = document.querySelector(".form-control").value
   // fetch posts after 1 seconds and reset loading when our posts have finished
   setTimeout(function(){
-   if(value > 100){
+   if(value > 100 || value > 10000){
     document.querySelector("#err").innerHTML = `<h2 class="text-center text-danger">API Not found</h2>`
     resetPosts()
+    document.querySelector(".form-custom-control").style.width = "auto"
   }else{
     fetchPosts()
     document.querySelector("#err").innerHTML = ""
+    document.querySelector(".form-custom-control").style.width = "5%"
   }
   resetLoading()
  },1000)
